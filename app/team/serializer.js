6ca944efc205846669179c7de4d2bd2b89b05b3e
Kyle Gosen <gosenkyl@msu.edu>
2017-09-26 23:31:19 -0400

    Team deserializer ids, not records

--- app/team/serializer.js
+++ app/team/serializer.js
@@ -3,7 +3,7 @@ import SportsAlarm from '../sports-alarm/serializer';
 export default SportsAlarm.extend({
 
   attrs: {
-    league: { key: "leagueId", serialize: "ids", deserialize: "records"},
+    league: { key: "leagueId", serialize: "ids", deserialize: "ids"},
     homeGames: {serialize: false, deserialize: 'records'},
     awayGames: {serialize: false, deserialize: 'records'}
   }
