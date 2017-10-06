892b04bdfeef5f06c8a484bd4afe28da29ac5469
Kyle Gosen <gosenkyl@msu.edu>
2017-10-06 01:46:50 -0400

    Added home icon

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('home', { path: '/'});
   this.route('leagues', { path: 'leagues/:league_id'});
   this.route('teams', { path: 'leagues/:league_id/teams/:team_id'});
 });
--- app/sports-alarm/adapter.js
+++ app/sports-alarm/adapter.js
@@ -1,8 +1,29 @@
+import Ember from 'ember';
 import DS from 'ember-data';
+
+let {
+  isEmpty
+  } = Ember;
 
 export default DS.RESTAdapter.extend({
 
   host: "http://localhost:8081",
-  namespace: "api"
+  namespace: "api",
+
+  shouldReloadAll(/*store, records*/) {
+    return false;
+  },
+
+  shouldBackgroundReloadAll(store, records) {
+    return isEmpty(records);
+  },
+
+  shouldReloadRecord(/*store, record*/) {
+    return false;
+  },
+
+  shouldBackgroundReloadRecord(store, record) {
+    return isEmpty(record);
+  }
 
 });
